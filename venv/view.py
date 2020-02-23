552d794f125af1abeefa5c46a3df80d0a120e52f
Elladan81 <heladan@hotmail.fr>
2020-02-23 18:19:40 +0100

    this commit change view delete test option show all categories and all products

--- venv/view.py
+++ venv/view.py
@@ -27,62 +27,6 @@ class View:
         self.count = []
         self.offset = 0
 
-    def show_all_categories(self, lim, off, table):
-        """ This method shows all the categories."""
-        self.run = 1
-        # change the limit and the offset for the table Categories
-        self.offset = off
-        while self.run:
-            # Get the name of the table Categories
-            name_table = list(table.keys())
-            cat = name_table[0]
-
-            # Count items in the table Categories
-            self.mycursor.execute("SELECT COUNT(*) FROM %s;" % cat)
-            for number in self.mycursor.fetchone():
-                tt_element = number
-
-            req = ("SELECT * FROM %s ORDER BY id LIMIT %s\
-                OFFSET %s;" % (cat, lim, self.offset))
-
-            self.display(
-                req, lim, self.offset, cat, tt_element,
-                config.SS_MENU, config.ERROR_1)
-
-            if self.u_choice == 'q' or self.u_choice == 'Q':
-                self.clear()
-                self.run = 0
-            elif self.u_choice.isdecimal():
-                self.show_all_products(self.u_choice, off, table)
-
-    def show_all_products(self, lim, off, table):
-        """ shows all products """
-        self.run = 1
-        # change the limit and the offset for the table Products
-        self.offset = off
-        while self.run:
-            # Get the name of the table Products
-            name_table = list(table.keys())
-            cat = name_table[1]
-
-            # Count how many items are there in the table Categories
-            self.mycursor.execute("SELECT COUNT(*) FROM %s;" % cat)
-            for number in self.mycursor.fetchone():
-                tt_element = number
-
-            req = ("SELECT id, name, brands FROM %s ORDER BY id LIMIT %s\
-                OFFSET %s;" % (cat, lim, self.offset))
-
-            self.display(
-                req, lim, self.offset, cat, tt_element,
-                config.SS_MENU_2, config.ERROR_1)
-
-            if self.u_choice == 'q' or self.u_choice == 'Q':
-                self.clear()
-                self.run = 0
-            elif self.u_choice.isdecimal():
-                self.more_information(self.u_choice)
-
     def find_substitute(self, lim, off, table):
         """
             allowing the user to find a better food
